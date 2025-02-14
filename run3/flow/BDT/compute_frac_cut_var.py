137e1fab8ae246ef6255bac4e1ee4d6b5a50d0f9
marcellocosti <marcellodicostanzo00@gmail.com>
2025-02-14 14:21:06 +0100

    factorize reweighting + implementing histo overwriting

--- run3/flow/BDT/compute_frac_cut_var.py
+++ run3/flow/BDT/compute_frac_cut_var.py
@@ -38,14 +38,8 @@ def minimise_chi2(config, ptmins, ptmaxs, hRawYields, hEffPrompt, hEffFD, output
     for iRow, row in enumerate(hCovCorrYields):
         for iCol, hCov in enumerate(row):
             SetObjectStyle(hCov, linecolor=kBlack)
-            if iRow == 0:
-                rowName = '#it{N}_{prompt}'
-            else:
-                rowName = '#it{N}_{non-prompt}'
-            if iCol == 0:
-                colName = '#it{N}_{prompt}'
-            else:
-                colName = '#it{N}_{non-prompt}'
+            rowName = '#it{N}_{prompt}' if iRow == 0 else '#it{N}_{non-prompt}'
+            colName = '#it{N}_{prompt}' if iCol == 0 else '#it{N}_{non-prompt}'
             hCov.SetTitle(f';#it{{p}}_{{T}} (GeV/#it{{c}}); #sigma({rowName}, {colName})')
 
     for iPt, (ptMin, ptMax) in enumerate(zip(ptmins, ptmaxs)):
